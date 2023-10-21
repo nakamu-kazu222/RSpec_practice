3cdce5fc9dbf3ac46c53260d4d763c65fe10c304
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-10-21 15:21:46 +0900

    テストコードを作成した
    タイトルが2文字以上10文字以下かつコンテンツが入力されている場合に、エラーが出ないことを確認する
    Post.new….でダミーデータを作成する
    「bundle exec rspec spec/models/post_spec.rb」でテストを実行する

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "タイトルと内容が入力されている場合" do
+    let!(:post) do
+      Post.new({title: "Rspecのテスト", content:"Rspecにトライしてみよう！"})
+    end
+    it '記事を保存できる' do
+      expect(post).to be_valid
+    end
+  end
 end
