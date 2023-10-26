d3f8e92e306bcd4e0ff4dde5c728d72f86072d4e
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-10-26 15:19:51 +0900

    Blogモデルのテストを作成した

--- myblog/spec/models/blog_spec.rb
+++ myblog/spec/models/blog_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @blog = Blog.new
+  end
+
+  it "バリデーションに失敗すること" do
+    @blog.should_not be_valid
+  end
 end
